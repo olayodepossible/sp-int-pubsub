b15dc96b26d0db0d444e8d1a41532a081f95d26f
olayode <aolayode@getcarbon.co>
2022-02-10 11:37:48 +0100

    implement rabbitMq integration

--- service/src/main/java/com/possible/sp/mail/ConfirmationMailTransformer.java
+++ service/src/main/java/com/possible/sp/mail/ConfirmationMailTransformer.java
@@ -8,9 +8,9 @@ public class ConfirmationMailTransformer {
 
     public String toMailText(RegistrationEvent event) {
         return "Dear " + event.getAttendeeFirstName() + " " + event.getAttendeeLastName() + ",\n\n" +
-                "Thank you for registering for the Globomantics Tech Conference. We are looking forward to meeting you!\n\n" +
+                "Thank you for registering for the SpTut Tech Conference. We are looking forward to meeting you!\n\n" +
                 "Your ticket code is: " + event.getTicketType() + "-" + event.getTicketCode() + "\n\n" +
                 "Sincerely,\n\n" +
-                "Globomantics Registration Team (registration@globomantics.com)";
+                "SpTut Registration Team.";
     }
 }
